3eb59197054a8a46caaa8ffc9bb740a63ef75b65
lantic <gytlantic@outlook.com>
2017-08-06 23:08:55 +0800

    modified:   index.css 	modified:   index.html 	modified:   index.js 	renamed:    res/icon/u59.png -> res/icon/jrwm.png 	renamed:    res/icon/u51.png -> res/icon/ykt.png 	modified:   shexiang/index.css 	modified:   shexiang/index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	var moreBtn = document.getElementById("more");
+	var ydcxBtn = document.getElementById("ydcx");
+	var yktBtn = document.getElementById("ykt");
+	var jrwmBtn = document.getElementById("jrwm");	
+	moreBtn.onclick = function () {
+		this.style.display = "none";
+		ydcxBtn.style.display = "block";
+		yktBtn.style.display = "block";
+		jrwmBtn.style.display = "block";
+	}
+
+});
