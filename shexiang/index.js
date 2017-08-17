9f88d66bd37dc22060a077ff496670b72e927da8
lantic <gytlantic@outlook.com>
2017-08-17 19:54:23 +0800

    modified:   shexiang/index.css 	modified:   shexiang/index.js

--- shexiang/index.js
+++ shexiang/index.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	function getPhoto() {
 		$.get("../api/posts", function (data,status) {
 			data.forEach(function (item) {
+				// create Img DOM
 				var img = $(`<img src=${item.thumb} hidden>`);
 				img.on("load", function () {
 					$(this).parent(".frame").addClass(img[0].naturalHeight > img[0].naturalWidth 
@@ -11,15 +12,70 @@ $(document).ready(function(){
 					$(this).prev().text("");
 					$(this).removeAttr("hidden");
 				})
+
+				// create LoveIcon DOM
 				var loveItem = "";
 				if(item.doilike){
 					loveItem = $(`<img src="src/icon/love-red.png" alt="love-item" class="love-icon">`)
 				} else {
 					loveItem = $(`<img src="src/icon/love-white.png" alt="love-item" class="love-icon">`)
 				}
-				loveItem.on("click", function () {
-					console.log(this.src);
+				loveItem.on("load", function () {
+					$(this).on("click", function (event) {
+						if (this.src.match(/white/)) {
+							this.src = "src/icon/love-red.png";
+							var loveNumber = $(this).parent().parent().next().children()[0].firstChild;
+							loveNumber.nodeValue = parseInt(loveNumber.nodeValue) + 1;
+							// send to bakend ...
+						} else {
+							this.src = "src/icon/love-white.png";
+							var loveNumber = $(this).parent().parent().next().children()[0].firstChild;
+							loveNumber.nodeValue = parseInt(loveNumber.nodeValue) - 1;
+							// send to bakend ...
+						}
+					})
 				})
+
+				//commitAction 
+				var commentItem = $(`<img src="src/icon/comment.png" alt="comment-icon">`);
+				commentItem.on("load", function () {
+					$(this).on("click",function () {
+						var formParent = this.parentNode.parentNode.parentNode.lastElementChild;
+						formParent.style.display = 'block';
+						var form = formParent.firstElementChild;
+						form.elements[0].focus();
+
+						$(form).on("submit", function (event) {
+							event.preventDefault();
+							// ajax...
+							var newCommentDom = $(`<div> 
+								<a href="#" style="display: block" class="user-comment">
+									<span class="author">MyName: </span>
+									<span>${this.elements[0].value}</span>
+								</a>
+								</div>`)
+
+							$(formParent).prev().append(newCommentDom);
+							form.elements[0].blur();
+							formParent.style.display = 'none';
+						})
+					})
+
+				})
+
+				// create otherUserComments DOM
+				var comments = item.comments;
+				var commentsContainer = $(`<div></div>`);
+				comments.forEach(function (comment) {
+					var ucObj = $(`<a href=${comment.user} style="display: block" class="user-comment"></a>`)
+					var uName = $(`<span class="author">${comment.name}: </span>`)
+					var uContent = $(`<span>${comment.content}</span>`)
+					ucObj.append(uName);
+					ucObj.append(uContent);
+					commentsContainer.append(ucObj);
+				})
+
+				// create article DOM
 				var article = $(`
 					<article class="photo-share" id="ta">
 						<header class="user">
@@ -44,7 +100,6 @@ $(document).ready(function(){
 							<div class="love-item">
 							</div>
 							<div class="comment-icon">
-								<img src="src/icon/comment.png" alt="comment-icon">
 							</div>
 						</div>
 
@@ -53,25 +108,20 @@ $(document).ready(function(){
 							<span>次赞</span>
 						</div>
 
-						<div class="communication">
-							<div class="user-comment">
-								<span class="author">lantic:</span>
-								<span>testtesdda</span>
-							</div>
-							<div class="user-comment">
-								<span class="author">lantic:</span>
-								<span>testtesdda</span>
-							</div>				
+						<div class="communication">			
 						</div>
 			 
 						<div class="comment-form">
 							<form action="">
 								<input type="text" placeholder="评论">
+								<input type="submit" style="display:none;"> 
 							</form>
 						</div> 
 					</article>`);
 
 				article.children(".frame").append(img);
+				article.children(".communication").append(commentsContainer);
+				article.find(".comment-icon").append(commentItem);
 				article.find(".love-item").append(loveItem);
 				$("#target").append(article);
 
@@ -103,28 +153,6 @@ $(document).ready(function(){
 		}
 	}, false);
 
-// love event listner
-	console.log(typeof getPhoto)
-
-	var loveBtnList = document.getElementsByClassName("love-item");
-	for (var i = 0; i < loveBtnList.length; ++i) {
-		loveBtnList[i].onclick = function () {
-			if (this.src.match(/white/)) {
-				this.src = "src/icon/love-red.png";
-			} else {
-				this.src = "src/icon/love-white.png";
-			}
-		}	
-	}
-
-
-//
-	var commentList = document.getElementsByClassName("comment-icon");
-	for (i = 0; i < commentList.length; ++i) {
-		commentList[i].onclick = function () {
-			this.parentNode.parentNode.lastElementChild.style.display = 'block';
-		}	
-	}
 
 	document.getElementById("image").onchange = function () {
 		var imgFile = this.files[0];
