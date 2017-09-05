5bf6dabc66cce15e2099df125c4ed5aab3d20343
lantic <gytlantic@outlook.com>
2017-09-05 09:45:54 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   shexiang/identify/index.css
    	modified:   shexiang/identify/index.html
    	new file:   shexiang/identify/index.js

---
+++ shexiang/identify/index.js
@@ -0,0 +1,16 @@
+$(function () {
+	document.getElementById("img").onchange = function () {
+		var imgFile = this.files[0];
+		var fReader = new FileReader();
+		fReader.onload = function () {
+			console.log(this.result);
+			var addImgNode = document.getElementById("addImg");
+			addImgNode.childNodes[2].nodeValue = "";
+			var prevImgNode = document.getElementById("preview-img");
+			prevImgNode.style.display = "inline-block";
+			prevImgNode.childNodes[1].src = fReader.result;
+		};
+		fReader.readAsDataURL(imgFile);	
+	};
+	
+});
